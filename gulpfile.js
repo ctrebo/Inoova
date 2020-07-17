5a48384ebdba24a79ff625be59cefb4f47ae358f
Trebo Christoffer <christoffer.trebo@gmail.com>
2020-07-17 08:31:18 +0200

    [ADD] began with animation

--- gulpfile.js
+++ gulpfile.js
@@ -40,7 +40,12 @@ function movefiles() {
     // Font Awesome Webfonts
     var fontAwesomeWebfonts = gulp.src('./node_modules/@fortawesome/fontawesome-free/webfonts/**/*')
         .pipe(gulp.dest('./vendor/fontawesome-free/webfonts'));
-    return merge(bootstrap, fontAwesomeCSS, fontAwesomeWebfonts)
+    var jquery = gulp.src([
+        './node_modules/jquery/dist/*',
+        '!./node_modules/jquery/dist/core.js'
+    ])
+    .pipe(gulp.dest('./vendor/jquery'));
+    return merge(bootstrap, fontAwesomeCSS, fontAwesomeWebfonts, jquery)
 }
 
 //watch scss and show changes in the browser
