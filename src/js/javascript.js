2c7046911bb26c2642a455bfdafd3077ebf7b3e6
Trebo Christoffer <christoffer.trebo@gmail.com>
2020-07-24 09:51:04 +0200

    [ADD] neuester stand

--- src/js/javascript.js
+++ src/js/javascript.js
@@ -69,41 +69,83 @@ var myVar;
 
 
 
-$( document ).ready(function() {
-    var x=window.scrollX;
-    var y=window.scrollY;
-    window.onscroll=function(){
-        if (y == 0) {
-            window.scrollTo(x, y);
-            document.getElementById("body").setAttribute('scroll', "no")
-            document.getElementById("body").setAttribute('style', "overflow: scroll;")
+// $( document ).ready(function() {
+//     var x=window.scrollX;
+//     var y=window.scrollY;
+//     window.onscroll=function(){
+//         if (y == 0) {
+//             window.scrollTo(x, y);
+//             document.getElementById("body").setAttribute('scroll', "no")
+//             document.getElementById("body").setAttribute('style', "overflow: scroll;")
             
 
-            document.getElementById("svg2").style.visibility = "visible";
-            document.getElementById("Ebene_1-2").classList.add("rotateall");
-            document.getElementById("Gruppe_411").classList.add("animationlogo");
-            document.getElementById("Ellipse_55").classList.add("Ellipse_1");
-            document.getElementById("Ellipse_55-2").classList.add("Ellipse_2");
-            document.getElementById("Ellipse_55-3").classList.add("Ellipse_3");
-            document.getElementById("Ellipse_55-4").classList.add("Ellipse_4");
+//             document.getElementById("svg2").style.visibility = "visible";
+//             document.getElementById("Ebene_1-2").classList.add("rotateall");
+//             document.getElementById("Gruppe_411").classList.add("animationlogo");
+//             document.getElementById("Ellipse_55").classList.add("Ellipse_1");
+//             document.getElementById("Ellipse_55-2").classList.add("Ellipse_2");
+//             document.getElementById("Ellipse_55-3").classList.add("Ellipse_3");
+//             document.getElementById("Ellipse_55-4").classList.add("Ellipse_4");
 
-            myVar = setTimeout(startScrolling, 1010);
-        } else  {
-            document.getElementById("svg2").style.visibility = "visible";
-            document.getElementById("Ellipse_55").style.visibility = "hidden";
-            document.getElementById("Ellipse_55-2").style.visibility = "hidden";
-            document.getElementById("Ellipse_55-3").style.visibility = "hidden";
-            document.getElementById("Ellipse_55-4").style.visibility = "hidden";
-        }
-    };
+//             myVar = setTimeout(startScrolling, 1010);
+//         } else  {
+//             document.getElementById("svg2").style.visibility = "visible";
+//             document.getElementById("Ellipse_55").style.visibility = "hidden";
+//             document.getElementById("Ellipse_55-2").style.visibility = "hidden";
+//             document.getElementById("Ellipse_55-3").style.visibility = "hidden";
+//             document.getElementById("Ellipse_55-4").style.visibility = "hidden";
+//         }
+//     };
+
+//     function startScrolling() {
+//         document.getElementById("body").removeAttribute("scroll");
+//         document.getElementById("body").setAttribute('style', "")
+//         window.onscroll = null;
+//     }
+
+// });
+
+
+
+$(document).ready(function(){
+    if(window.matchMedia("(max-width: 768px)").matches){
+        document.getElementById("svg2").style.visibility = "visible";
+        document.getElementById("Ellipse_55").style.visibility = "hidden";
+        document.getElementById("Ellipse_55-2").style.visibility = "hidden";
+        document.getElementById("Ellipse_55-3").style.visibility = "hidden";
+        document.getElementById("Ellipse_55-4").style.visibility = "hidden";
+    } else {
+        var x=window.scrollX;
+        var y=window.scrollY;
+        window.onscroll=function(){
+            if (y == 0) {
+                window.scrollTo(x, y);
+                document.getElementById("body").setAttribute('scroll', "no")
+                document.getElementById("body").setAttribute('style', "overflow: scroll;")
+                
+
+                document.getElementById("svg2").style.visibility = "visible";
+                document.getElementById("Ebene_1-2").classList.add("rotateall");
+                document.getElementById("Gruppe_411").classList.add("animationlogo");
+                document.getElementById("Ellipse_55").classList.add("Ellipse_1");
+                document.getElementById("Ellipse_55-2").classList.add("Ellipse_2");
+                document.getElementById("Ellipse_55-3").classList.add("Ellipse_3");
+                document.getElementById("Ellipse_55-4").classList.add("Ellipse_4");
+
+                myVar = setTimeout(startScrolling, 1010);
+            } 
+        };
 
     function startScrolling() {
         document.getElementById("body").removeAttribute("scroll");
         document.getElementById("body").setAttribute('style', "")
         window.onscroll = null;
-}
+        }
+    }
+
 
 });
+
 
 
 /////////////////////////////////////////////////////////////////////////////
